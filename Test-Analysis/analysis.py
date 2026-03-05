d15f1934a565106426b25f585b67d79c6e3d0ad3
Alice Sharam <alice.sharam@pm.me>
2026-03-05 14:56:21 +0000

    Added file counts

--- Test-Analysis/analysis.py
+++ Test-Analysis/analysis.py
@@ -2,15 +2,20 @@ import os
 
 directories = ["With-Moves", "Without-Moves"]
 cycles = {}
+file_counts = {}
 for directory_name in directories:
     cycles[directory_name] = 0
+    file_counts[directory_name] = 0
     directory = os.fsencode(directory_name)
     for filename in os.listdir(directory):
         filepath = os.path.join(directory_name, os.fsdecode(filename))
         with open(filepath, "r") as file:
+            file_counts[directory_name] += 1
             lines = file.readlines()
             n = len(lines)
             cycles[directory_name] += int(lines[n-2].split(":")[0])
-    print(directory_name + ":\nCycles: " + str(cycles[directory_name]))
+    print(directory_name + ":")
+    print("File count: " + str(file_counts[directory_name]))
+    print("Total Cycles: " + str(cycles[directory_name]))
     
 print("Summary:\nPercent reduction: " + str((-(cycles[directories[0]]/cycles[directories[1]]) + 1)*100) + "%")
